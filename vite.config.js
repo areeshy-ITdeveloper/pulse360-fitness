7f1e2c49c0e3ad28c9139b894e5875cc32f84c30
areeshy-ITdeveloper <areeshyit@gmail.com>
2026-09-17 17:34:14 +0500

    Fix GitHub Pages base path for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pulse360-fitness/', // Repository ka exact name
   server: {
-    port: 8080, // Aap apni pasand ka port number (e.g. 3000, 8080) yahan likh sakti hain
+    port: 8080,
   }
 })
